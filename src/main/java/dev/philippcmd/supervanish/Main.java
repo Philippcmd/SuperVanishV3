b102e6a0f428430dddf24d5ab9eacfd41ee7fa7f
Philippcmd <139921614+Philippcmd@users.noreply.github.com>
2025-02-06 18:30:17 +0100

    Added 'TAB Completer' for --all

--- src/main/java/dev/philippcmd/supervanish/Main.java
+++ src/main/java/dev/philippcmd/supervanish/Main.java
@@ -4,7 +4,9 @@ import dev.philippcmd.supervanish.commands.VanishCommand;
 import dev.philippcmd.supervanish.commands.SuperVanishCommand;
 import dev.philippcmd.supervanish.commands.VanishShowCommand;
 import dev.philippcmd.supervanish.commands.VanishListCommand;
+import dev.philippcmd.supervanish.tabcompleters.VanishShowTabCompleter;
 import dev.philippcmd.supervanish.utils.VanishManager;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -19,6 +21,8 @@ public class Main extends JavaPlugin {
         getCommand("supervanish").setExecutor(new SuperVanishCommand(vanishManager));
         getCommand("vanish-show").setExecutor(new VanishShowCommand(vanishManager));
         getCommand("vanish-list").setExecutor(new VanishListCommand(vanishManager));
+
+        getCommand("vanish-show").setTabCompleter(new VanishShowTabCompleter());
 
         getLogger().info("SuperVanish Plugin enabled!");
     }
---
+++ src/main/java/dev/philippcmd/supervanish/tabcompleters/VanishShowTabCompleter.java
@@ -0,0 +1,19 @@
+package dev.philippcmd.supervanish.tabcompleters;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.Collections;
+import java.util.List;
+
+public class VanishShowTabCompleter implements TabCompleter {
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            return Collections.singletonList("--all");
+        }
+        return Collections.emptyList();
+    }
+}
